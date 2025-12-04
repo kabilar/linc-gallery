9fb911cd18e4f04946067fa8639814dfe1d59c9d
Kabilar Gunalan <kabi@mit.edu>
2025-12-04 12:25:46 -0600

    Add homepage to nav

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,6 +41,7 @@ const config = {
         items: [
           {to: '/pathways', label: 'Pathway Atlas', position: 'right'},
           {to: '/dmri', label: 'High-resolution dMRI', position: 'right'},
+          {to: 'https://connects.mgh.harvard.edu/', label: 'Project Homepage', position: 'right'},
         ],
       },
       footer: {
